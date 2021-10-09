73564538816668cf1925b6e2d0b0def782a92c94
ryunakae <ryunakae@gmail.com>
2021-10-09 15:24:53 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>CHESS</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
